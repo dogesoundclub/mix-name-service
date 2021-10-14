0fcd67fa7260a19819cb9aeaa536354ea26e8ce9
Hanul <hanul@hanul.me>
2021-10-15 00:06:33 +0900

    계약 작성 완료

--- contracts/MixNameService.sol
+++ contracts/MixNameService.sol
@@ -1,9 +1,66 @@
 pragma solidity ^0.5.6;
 
+import "./klaytn-contracts/token/KIP17/IKIP17.sol";
 import "./klaytn-contracts/ownership/Ownable.sol";
 import "./klaytn-contracts/math/SafeMath.sol";
 import "./interfaces/IMixNameService.sol";
+import "./interfaces/IMix.sol";
 
 contract MixNameService is Ownable, IMixNameService {
     using SafeMath for uint256;
+
+    IMix public mix;
+
+    constructor(IMix _mix) public {
+        mix = _mix;
+    }
+
+    uint256 public mixForChanging = 100 * 1e18;
+    uint256 public mixForDeleting = 200 * 1e18;
+
+    mapping(address => string) public names;
+    mapping(address => bool) public named;
+    mapping(string => bool) public _exists;
+
+    function setMixForChanging(uint256 _mix) onlyOwner external {
+        mixForChanging = _mix;
+        emit SetMixForChanging(_mix);
+    }
+
+    function setMixForDeleting(uint256 _mix) onlyOwner external {
+        mixForDeleting = _mix;
+        emit SetMixForDeleting(_mix);
+    }
+
+    function exists(string calldata name) view external returns (bool) {
+        return _exists[name];
+    }
+
+    function set(string calldata name) external {
+        require(_exists[name] != true);
+
+        if (named[msg.sender] == true) {
+            _exists[names[msg.sender]] = false;
+        } else {
+            named[msg.sender] = true;
+        }
+
+        names[msg.sender] = name;
+        _exists[name] = true;
+        
+        mix.burnFrom(msg.sender, mixForChanging);
+
+        emit Set(msg.sender, name);
+    }
+
+    function remove() external {
+
+        delete _exists[names[msg.sender]];
+        delete names[msg.sender];
+        delete named[msg.sender];
+
+        mix.burnFrom(msg.sender, mixForDeleting);
+
+        emit Remove(msg.sender);
+    }
 }
--- contracts/interfaces/IMixNameService.sol
+++ contracts/interfaces/IMixNameService.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.5.6;
 
+import "../klaytn-contracts/token/KIP17/IKIP17.sol";
+
 interface IMixNameService {
-    
+
+    event SetMixForChanging(uint256 _mix);
+    event SetMixForDeleting(uint256 _mix);
+
+    event Set(address indexed owner, string name);
+    event Remove(address indexed owner);
+
+    function mix() view external returns (IKIP17);
+    function mixForChanging() view external returns (uint256);
+    function mixForDeleting() view external returns (uint256);
+
+    function names(address owner) view external returns (string memory name);
+    function named(address owner) view external returns (bool);
+    function exists(string calldata name) view external returns (bool);
+    function set(string calldata name) external;
+    function remove() external;
 }
